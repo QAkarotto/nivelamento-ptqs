8bf4c6bead7a5ea9639a402c121b8274f2742a02
Goku (João Vitor dos Santos) <qakarotto@gmail.com>
2022-10-28 18:36:52 -0300

    feature(modulo6): Variáveis

--- src/main/java/br/com/lojinha/LojinhaApp.java
+++ src/main/java/br/com/lojinha/LojinhaApp.java
@@ -2,6 +2,14 @@ package br.com.lojinha;
 
 public class LojinhaApp {
     public static void main(String[] args) {
-        System.out.println("Teste");
+        String joias = "100gr de ouro 50k";
+        int idade = 24;
+        double salario = 49500.99;
+        boolean resposta = false;
+
+        System.out.println(joias);
+        System.out.println(idade);
+        System.out.println(salario);
+        System.out.println(resposta);
     }
 }
